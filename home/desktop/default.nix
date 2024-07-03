f6b94a3ead315b173d5f3f678cf9e4f47749ab4f
marshmallow <marshycity@gmail.com>
2024-07-03 12:25:29 +0000

    prettier sweep

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,4 +1,8 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [./wm];
 
   gtk = {
@@ -11,18 +15,18 @@
 
   systemd.user.services.keepass = {
     Unit = {
-        After = ["network.target"];
-        Wants = ["network.target"];
+      After = ["network.target"];
+      Wants = ["network.target"];
     };
     Service = {
-        Type = "notify";
-        ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} -p %h/.local/share/keepass";
-        ExecStart = "${lib.getExe pkgs.rclone} mount google-drive:/Keepass %h/.local/share/keepass --allow-other --log-level INFO";
-        ExecStop = "${lib.getExe' pkgs.fuse "fusermount"} -u %h/.local/share/keepass";
-        Environment = [ "PATH=/run/wrappers/bin/" ];
+      Type = "notify";
+      ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} -p %h/.local/share/keepass";
+      ExecStart = "${lib.getExe pkgs.rclone} mount google-drive:/Keepass %h/.local/share/keepass --allow-other --log-level INFO";
+      ExecStop = "${lib.getExe' pkgs.fuse "fusermount"} -u %h/.local/share/keepass";
+      Environment = ["PATH=/run/wrappers/bin/"];
     };
     Install = {
-        WantedBy = ["default.target"];
+      WantedBy = ["default.target"];
     };
   };
 }
