c3973d5deeb255790cde88ae5ee34641ae24555e
marshmallow <marshycity@gmail.com>
2024-07-06 15:18:22 +1000

    add makeRcloneMount

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -2,7 +2,24 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  makeRcloneMount = remotePath: localPath: {
+    Unit = {
+      After = ["network.target"];
+      Wants = ["network.target"];
+    };
+    Service = {
+      Type = "notify";
+      ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} -p ${localPath}";
+      ExecStart = "${lib.getExe pkgs.rclone} mount google-drive:${remotePath} ${localPath} --allow-other --log-level INFO";
+      ExecStop = "${lib.getExe' pkgs.fuse "fusermount"} -u ${localPath}";
+      Environment = ["PATH=/run/wrappers/bin/"];
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+  };
+in {
   imports = [./wm];
 
   gtk = {
@@ -13,20 +30,5 @@
     };
   };
 
-  systemd.user.services.keepass = {
-    Unit = {
-      After = ["network.target"];
-      Wants = ["network.target"];
-    };
-    Service = {
-      Type = "notify";
-      ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} -p %h/.local/share/keepass";
-      ExecStart = "${lib.getExe pkgs.rclone} mount google-drive:/Keepass %h/.local/share/keepass --allow-other --log-level INFO";
-      ExecStop = "${lib.getExe' pkgs.fuse "fusermount"} -u %h/.local/share/keepass";
-      Environment = ["PATH=/run/wrappers/bin/"];
-    };
-    Install = {
-      WantedBy = ["default.target"];
-    };
-  };
+  systemd.user.services.keepass = makeRcloneMount "/Keepass" "%h/.local/share/keepass";
 }
