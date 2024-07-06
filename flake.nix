17e87c61f815b2e8b44c01877a2f6a4c7dc9c0f0
marshmallow <marshycity@gmail.com>
2024-07-06 17:04:48 +1000

    use lix `main`

--- flake.nix
+++ flake.nix
@@ -11,12 +11,12 @@
     candy.url = "github:mrshmllow/nvim-candy";
 
     lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/release-2.90.tar.gz";
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
     };
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.90.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.lix.follows = "lix";
     };
