9af9232e22eb1ecd96df19132b4fab09360c837b
marshmallow <marshycity@gmail.com>
2024-08-15 18:50:29 +1000

    minecraft-server: use velocity

--- nodes/minecraft-server/default.nix
+++ nodes/minecraft-server/default.nix
@@ -2,10 +2,12 @@
   inputs,
   pkgs,
   lib,
+  config,
   ...
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./module.nix
   ];
 
   nixpkgs = {
@@ -17,66 +19,139 @@
       ];
   };
 
-  services.minecraft-servers = {
+  services.minecraft-servers = let
+    fabric = pkgs.fabricServers.fabric-1_21_1;
+    mods = {
+      "mods/FabricProxy-Lite.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/8dI2tmqs/versions/AQhF7kvw/FabricProxy-Lite-2.9.0.jar";
+        hash = "sha256-wIQA86Uh6gIQgmr8uAJpfWY2QUIBlMrkFu0PhvQPoac=";
+      };
+      "mods/Lithium.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/5szYtenV/lithium-fabric-mc1.21.1-0.13.0.jar";
+        hash = "sha256-ENNx/uOXvwMG4eLYY8VMVkQrzC3G4BYD8UafL+SRDWE=";
+      };
+      "mods/FerriteCore.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/uXXizFIs/versions/wmIZ4wP4/ferritecore-7.0.0-fabric.jar";
+        hash = "sha256-LDEgDR9d5qPPXtxMPTkgBjbh4GDEtUjc+CSe9IdmAyM=";
+      };
+      "mods/SimpleVoiceChat.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/lZkOuATd/voicechat-fabric-1.21.1-2.5.20.jar";
+        hash = "sha256-YDFhGhT34iNWUT7jYBGn2A/E63RxhRZ6Jtdxzyf1y2U=";
+      };
+      "mods/Chunky.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/fALzjamp/versions/dPliWter/Chunky-1.4.16.jar";
+        hash = "sha256-yfA+Mi5jHulMy42/N3aFnNEnZuUTt1M+n5ZueZ20eTc=";
+      };
+      "mods/Fabric.jar" = pkgs.fetchurl {
+        url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/bK6OgzFj/fabric-api-0.102.1%2B1.21.1.jar";
+        hash = "sha256-2ouVZNKtpqT/6uYrwSn7QhRHhsxTzx55oNcj8vaTopw=";
+      };
+      "mods/LuckPerms.jar" = pkgs.fetchurl {
+        url = "https://download.luckperms.net/1554/fabric/LuckPerms-Fabric-5.4.139.jar";
+        hash = "sha256-QOdw3jgJnYXOZuaVp45z0jzRH1Ar56FZwm4onJzBEtQ=";
+      };
+    };
+
+    whitelist = {
+      marshmallow = "dd4edde3-f0c7-4e27-82df-9c374548f2b9";
+
+      Tentable876 = "665d064b-2b07-420d-859b-7f1774b46568";
+      pepsi985 = "28035660-6a73-44a6-b3a6-022f52955a5b";
+      Pepsi_max5901 = "3a5c4759-6d18-473e-a397-201a7b84410d";
+      Always_Happy213 = "8b146eea-ef23-40ea-b8fd-4b8b592a2969";
+      MaterialTec = "ce4f5c09-bb90-45c6-ae1c-28ebe17b39af";
+      Polarite_Panda = "307d67f3-7a4c-4853-b3a8-d8d78c1f90c8";
+    };
+  in {
     enable = true;
     eula = true;
 
-    servers.default = {
+    voicechat-servers = ["default" "creative"];
+
+    velocity = {
       enable = true;
-      autoStart = true;
       openFirewall = true;
-      package = pkgs.fabricServers.fabric-1_21_1;
+      config = {
+        servers = ["default" "creative"];
+        try = ["default" "creative"];
+        motd = "visit <color:#4287f5>mc.althaea.zone</color:#4287f5> for the map";
+      };
+    };
 
-      whitelist = {
-        marshmallow = "dd4edde3-f0c7-4e27-82df-9c374548f2b9";
-
-        Tentable876 = "665d064b-2b07-420d-859b-7f1774b46568";
-        pepsi985 = "28035660-6a73-44a6-b3a6-022f52955a5b";
-        Pepsi_max5901 = "3a5c4759-6d18-473e-a397-201a7b84410d";
-        Always_Happy213 = "8b146eea-ef23-40ea-b8fd-4b8b592a2969";
-        MaterialTec = "ce4f5c09-bb90-45c6-ae1c-28ebe17b39af";
-        Polarite_Panda = "307d67f3-7a4c-4853-b3a8-d8d78c1f90c8";
+    servers = {
+      velocity = {
+        symlinks = {
+          "plugins/LuckPerms-Velocity.jar" = pkgs.fetchurl {
+            url = "https://download.luckperms.net/1554/velocity/LuckPerms-Velocity-5.4.139.jar";
+            hash = "sha256-6v/CtD8QeqSuU9Y7oTIRJgIZCEiUrCOeslk52Euf38Y=";
+          };
+          "plugins/luckperms/config.yml" = ./lp.velocity.yml;
+        };
       };
 
-      serverProperties = {
-        white-list = true;
-        difficulty = "normal";
-        motd = "visit https://mc.althaea.zone for the map";
+      creative = {
+        enable = true;
+        autoStart = true;
+        # Do not open!
+        openFirewall = false;
+        package = fabric;
+
+        inherit whitelist;
+
+        serverProperties = {
+          white-list = true;
+          difficulty = "normal";
+          gamemode = "creative";
+          level-type = "minecraft:flat";
+          server-port = 25567;
+        };
+
+        symlinks =
+          {
+            "config/luckperms/luckperms.conf" = pkgs.runCommand "luckperms.conf" {} ''
+              cp ${./lp.fabric.conf} $out
+              substituteInPlace $out --replace "%SERVER%" "creative"
+            '';
+          }
+          // mods;
       };
 
-      symlinks = {
-        mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
-          Lithium = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/5szYtenV/lithium-fabric-mc1.21.1-0.13.0.jar";
-            hash = "sha256-ENNx/uOXvwMG4eLYY8VMVkQrzC3G4BYD8UafL+SRDWE=";
-          };
-          FerriteCore = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/uXXizFIs/versions/wmIZ4wP4/ferritecore-7.0.0-fabric.jar";
-            hash = "sha256-LDEgDR9d5qPPXtxMPTkgBjbh4GDEtUjc+CSe9IdmAyM=";
-          };
-          SimpleVoiceChat = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/lZkOuATd/voicechat-fabric-1.21.1-2.5.20.jar";
-            hash = "sha256-YDFhGhT34iNWUT7jYBGn2A/E63RxhRZ6Jtdxzyf1y2U=";
-          };
-          BlueMap = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/swbUV1cr/versions/Zpzf0Xab/BlueMap-5.3-fabric.jar";
-            hash = "sha256-5FdRX2773zgIR5MQSNAO52lblA5M9wi57fjbQSe0oXs=";
-          };
-          Chunky = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/fALzjamp/versions/dPliWter/Chunky-1.4.16.jar";
-            hash = "sha256-yfA+Mi5jHulMy42/N3aFnNEnZuUTt1M+n5ZueZ20eTc=";
-          };
-          Fabric = pkgs.fetchurl {
-            url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/bK6OgzFj/fabric-api-0.102.1%2B1.21.1.jar";
-            hash = "sha256-2ouVZNKtpqT/6uYrwSn7QhRHhsxTzx55oNcj8vaTopw=";
-          };
-        });
+      default = {
+        enable = true;
+        autoStart = true;
+        # Do not open!
+        openFirewall = false;
+        package = fabric;
+
+        inherit whitelist;
+
+        serverProperties = {
+          white-list = true;
+          difficulty = "normal";
+          server-port = 25568;
+        };
+
+        symlinks =
+          {
+            "config/luckperms/luckperms.conf" = pkgs.runCommand "luckperms.conf" {} ''
+              cp ${./lp.fabric.conf} $out
+              substituteInPlace $out --replace "%SERVER%" "survival"
+            '';
+
+            "mods/BlueMap.jar" = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/swbUV1cr/versions/Zpzf0Xab/BlueMap-5.3-fabric.jar";
+              hash = "sha256-5FdRX2773zgIR5MQSNAO52lblA5M9wi57fjbQSe0oXs=";
+            };
+          }
+          // mods;
       };
     };
   };
 
-  # Simple Voice Chat
-  networking.firewall.allowedUDPPorts = [24454];
+  # caddy
+  networking.firewall.allowedUDPPorts = [config.services.minecraft-servers.velocity.port];
+
+  networking.firewall.allowedTCPPorts = [80 443];
 
   services.caddy = {
     enable = true;
@@ -89,7 +164,16 @@
     '';
   };
 
-  networking.firewall.allowedTCPPorts = [80 443];
+  services.postgresql = {
+    enable = true;
+    ensureUsers = [
+      {
+        name = "minecraft";
+        ensureDBOwnership = true;
+      }
+    ];
+    ensureDatabases = ["minecraft"];
+  };
 
   environment.systemPackages = with pkgs; [
     tmux
---
+++ nodes/minecraft-server/module.nix
@@ -0,0 +1,272 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.minecraft-servers;
+in {
+  options.services.minecraft-servers = {
+    voicechat-servers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+    };
+
+    velocity = {
+      enable = lib.mkEnableOption "velocity server";
+
+      openFirewall = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 25565;
+      };
+
+      config = {
+        motd = lib.mkOption {
+          type = lib.types.str;
+          default = "A minecraft server";
+        };
+
+        servers = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [];
+        };
+
+        try = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [];
+        };
+      };
+    };
+  };
+
+  config = {
+    services.minecraft-servers.servers = lib.mkMerge [
+      (lib.genAttrs cfg.velocity.config.servers (name: {
+        serverProperties = {
+          white-list = true;
+          online-mode = false;
+          server-ip = "127.0.0.1";
+        };
+      }))
+      (lib.genAttrs cfg.voicechat-servers (name: {
+        symlinks = {
+          "config/voicechat/voicechat-server.properties" = pkgs.writeText "voicechat-server.properties" ''
+            # Setting this to "-1" sets the port to the Minecraft servers port (Not recommended)
+            port=${builtins.toString ((lib.lists.findFirstIndex (n: n == name) null cfg.voicechat-servers) + 20000)}
+            # Leave empty to use 'server-ip' of server.properties
+            bind_address=
+            # The distance to where the voice can be heard
+            max_voice_distance=48.0
+            # The multiplier of the voice distance when crouching
+            crouch_distance_multiplier=1.0
+            # The multiplier of the voice distance when whispering
+            whisper_distance_multiplier=0.5
+            # The opus codec
+            # Possible values are 'VOIP', 'AUDIO' and 'RESTRICTED_LOWDELAY'
+            codec=VOIP
+            # The maximum size in bytes that voice packets are allowed to have
+            mtu_size=1024
+            # The frequency at which keep alive packets are sent
+            # Setting this to a higher value may result in timeouts
+            keep_alive=1000
+            # If group chats are allowed
+            enable_groups=true
+            # The host name that clients should use to connect to the voice chat
+            # This may also include a port, e.g. 'example.com:24454'
+            # Don't change this value if you don't know what you are doing
+            voice_host=
+            # If players are allowed to record the voice chat
+            allow_recording=true
+            # If spectators are allowed to talk to other players
+            spectator_interaction=false
+            # If spectators can talk to players they are spectating
+            spectator_player_possession=false
+            # If players without the mod should get kicked from the server
+            force_voice_chat=false
+            # The amount of milliseconds, the server should wait to check if the player has the mod installed
+            # Only active when force_voice_chat is set to true
+            login_timeout=10000
+            # The range where the voice chat should broadcast audio to
+            # A value <0 means 'max_voice_distance'
+            broadcast_range=-1.0
+            # If the voice chat server should reply to pings
+            allow_pings=true
+          '';
+        };
+      }))
+      {
+        velocity = lib.mkIf cfg.velocity.enable {
+          enable = cfg.velocity.enable;
+          autoStart = true;
+          package = pkgs.velocityServers.velocity;
+
+          # https://docs.papermc.io/velocity/tuning#tune-your-startup-flags
+          jvmOpts = "-XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15";
+
+          symlinks = {
+            "plugins/SimpleVoiceChatVelocity.jar" = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/svvcJhgC/voicechat-velocity-2.5.20.jar";
+              hash = "sha256-GFMTzvlvfZAItpYAlzLUPbWTBZUarufgmjopTEQviFs=";
+            };
+            "velocity.toml" = let
+              serverEntry = name: ''${name} = "${cfg.servers.${name}.serverProperties.server-ip}:${builtins.toString cfg.servers.${name}.serverProperties.server-port}"'';
+              serverEntries = lib.concatStringsSep "\n" (map serverEntry cfg.velocity.config.servers);
+              tryList = lib.concatStringsSep ", " (map (s: ''"${s}"'') cfg.velocity.config.try);
+            in
+              pkgs.writeText "velocity.toml" ''
+                # Do not change this
+                config-version = "2.7"
+
+                bind = "0.0.0.0:${builtins.toString cfg.velocity.port}"
+
+                # MiniMessage format
+                motd = "${cfg.velocity.config.motd}"
+
+                # What should we display for the maximum number of players? (Velocity does not support a cap
+                # on the number of players online.)
+                show-max-players = 20
+
+                # Should we authenticate players with Mojang? By default, this is on.
+                online-mode = true
+
+                # Should the proxy enforce the new public key security standard? By default, this is on.
+                force-key-authentication = true
+
+                # If client's ISP/AS sent from this proxy is different from the one from Mojang's
+                # authentication server, the player is kicked. This disallows some VPN and proxy
+                # connections but is a weak form of protection.
+                prevent-client-proxy-connections = false
+
+                # Should we forward IP addresses and other data to backend servers?
+                # Available options:
+                # - "none":        No forwarding will be done. All players will appear to be connecting
+                #                  from the proxy and will have offline-mode UUIDs.
+                # - "legacy":      Forward player IPs and UUIDs in a BungeeCord-compatible format. Use this
+                #                  if you run servers using Minecraft 1.12 or lower.
+                # - "bungeeguard": Forward player IPs and UUIDs in a format supported by the BungeeGuard
+                #                  plugin. Use this if you run servers using Minecraft 1.12 or lower, and are
+                #                  unable to implement network level firewalling (on a shared host).
+                # - "modern":      Forward player IPs and UUIDs as part of the login process using
+                #                  Velocity's native forwarding. Only applicable for Minecraft 1.13 or higher.
+                player-info-forwarding-mode = "modern"
+
+                # If you are using modern or BungeeGuard IP forwarding, configure a file that contains a unique secret here.
+                # The file is expected to be UTF-8 encoded and not empty.
+                forwarding-secret-file = "forwarding.secret"
+
+                # Announce whether or not your server supports Forge. If you run a modded server, we
+                # suggest turning this on.
+                #
+                # If your network runs one modpack consistently, consider using ping-passthrough = "mods"
+                # instead for a nicer display in the server list.
+                announce-forge = false
+
+                # If enabled (default is false) and the proxy is in online mode, Velocity will kick
+                # any existing player who is online if a duplicate connection attempt is made.
+                kick-existing-players = false
+
+                # Should Velocity pass server list ping requests to a backend server?
+                # Available options:
+                # - "disabled":    No pass-through will be done. The velocity.toml and server-icon.png
+                #                  will determine the initial server list ping response.
+                # - "mods":        Passes only the mod list from your backend server into the response.
+                #                  The first server in your try list (or forced host) with a mod list will be
+                #                  used. If no backend servers can be contacted, Velocity won't display any
+                #                  mod information.
+                # - "description": Uses the description and mod list from the backend server. The first
+                #                  server in the try (or forced host) list that responds is used for the
+                #                  description and mod list.
+                # - "all":         Uses the backend server's response as the proxy response. The Velocity
+                #                  configuration is used if no servers could be contacted.
+                ping-passthrough = "DISABLED"
+
+                # If not enabled (default is true) player IP addresses will be replaced by <ip address withheld> in logs
+                enable-player-address-logging = true
+
+                [servers]
+                ${serverEntries}
+
+                try = [${tryList}]
+
+                [forced-hosts]
+
+                [advanced]
+                # How large a Minecraft packet has to be before we compress it. Setting this to zero will
+                # compress all packets, and setting it to -1 will disable compression entirely.
+                compression-threshold = 256
+
+                # How much compression should be done (from 0-9). The default is -1, which uses the
+                # default level of 6.
+                compression-level = -1
+
+                # How fast (in milliseconds) are clients allowed to connect after the last connection? By
+                # default, this is three seconds. Disable this by setting this to 0.
+                login-ratelimit = 3000
+
+                # Specify a custom timeout for connection timeouts here. The default is five seconds.
+                connection-timeout = 5000
+
+                # Specify a read timeout for connections here. The default is 30 seconds.
+                read-timeout = 30000
+
+                # Enables compatibility with HAProxy's PROXY protocol. If you don't know what this is for, then
+                # don't enable it.
+                haproxy-protocol = false
+
+                # Enables TCP fast open support on the proxy. Requires the proxy to run on Linux.
+                tcp-fast-open = false
+
+                # Enables BungeeCord plugin messaging channel support on Velocity.
+                bungee-plugin-message-channel = true
+
+                # Shows ping requests to the proxy from clients.
+                show-ping-requests = false
+
+                # By default, Velocity will attempt to gracefully handle situations where the user unexpectedly
+                # loses connection to the server without an explicit disconnect message by attempting to fall the
+                # user back, except in the case of read timeouts. BungeeCord will disconnect the user instead. You
+                # can disable this setting to use the BungeeCord behavior.
+                failover-on-unexpected-server-disconnect = true
+
+                # Declares the proxy commands to 1.13+ clients.
+                announce-proxy-commands = true
+
+                # Enables the logging of commands
+                log-command-executions = false
+
+                # Enables logging of player connections when connecting to the proxy, switching servers
+                # and disconnecting from the proxy.
+                log-player-connections = true
+
+                # Allows players transferred from other hosts via the
+                # Transfer packet (Minecraft 1.20.5) to be received.
+                accepts-transfers = false
+
+                [query]
+                # Whether to enable responding to GameSpy 4 query responses or not.
+                enabled = false
+
+                # If query is enabled, on what port should the query protocol listen on?
+                port = 25566
+
+                # This is the map name that is reported to the query services.
+                map = "Velocity"
+
+                # Whether plugins should be shown in query response by default or not
+                show-plugins = false
+              '';
+          };
+        };
+      }
+    ];
+
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.velocity.openFirewall [
+      cfg.velocity.port
+    ];
+  };
+}
