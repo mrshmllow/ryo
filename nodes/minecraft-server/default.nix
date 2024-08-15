385d1ad3bdd703400006cd5589e0b0b6f9a51b0c
marshmallow <marshycity@gmail.com>
2024-08-15 13:12:04 +1000

    minecraft-server: add marsh-framework ssh key

--- nodes/minecraft-server/default.nix
+++ nodes/minecraft-server/default.nix
@@ -100,6 +100,6 @@
   networking.hostName = "mc";
   networking.domain = "althaea.zone";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2kH40sa5FDliqDNROQgsR5h1W6sdSona22K9YvUx0K marsh@marsh-wsl''];
+  users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2kH40sa5FDliqDNROQgsR5h1W6sdSona22K9YvUx0K marsh@marsh-wsl" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsrE2dWjrj+nBTYrrfpVIaW6wxs3ClSDW3iKffD73p+ marsh@marsh-framework"];
   system.stateVersion = "23.11";
 }
