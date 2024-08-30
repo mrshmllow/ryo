3381e6d2dd3319ca2e737820e355882e9be32d5b
marshmallow <marshycity@gmail.com>
2024-08-30 10:21:33 +1000

    outpost-3: add framework wsl public key

--- nodes/outpost-3/default.nix
+++ nodes/outpost-3/default.nix
@@ -14,6 +14,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsrE2dWjrj+nBTYrrfpVIaW6wxs3ClSDW3iKffD73p+ marsh@marsh-framework"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFCPRaZzp7mPqlyJo6c5VW/hBHlzHwo2oyrZhdNxmdY marsh@marsh-wsl"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2kH40sa5FDliqDNROQgsR5h1W6sdSona22K9YvUx0K marsh@marsh-wsl"
   ];
   system.stateVersion = "23.11";
 }
