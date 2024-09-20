64d00b048b833c990ff3760b9985cc0f6dd524ce
marshmallow <marshycity@gmail.com>
2024-09-20 21:25:00 +1000

    use offical images for gitea runner

--- nodes/outpost-3/services/git.althaea.zone/runner.nix
+++ nodes/outpost-3/services/git.althaea.zone/runner.nix
@@ -3,7 +3,7 @@
     name = "outpost-3";
     enable = true;
     labels = [
-      "ubuntu-latest:docker://node:21-bullseye"
+      "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest"
     ];
     tokenFile = "/etc/keys/gitea-actions-runner.env";
     url = "https://git.althaea.zone";
