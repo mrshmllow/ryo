b4432fbe9f21bb874af1ad88f3501e82761340fd
marshmallow <marshycity@gmail.com>
2025-01-01 10:36:39 +1100

    re-add diff activiation script

--- modules/default.nix
+++ modules/default.nix
@@ -12,4 +12,17 @@
       type = lib.types.listOf lib.types.str;
     };
   };
+
+  config = {
+    system.activationScripts.diff = {
+      supportsDryActivation = true;
+      text = ''
+        if [ -e /run/current-system ]; then
+          PATH=${lib.makeBinPath [config.nix.package]} \
+            ${lib.getExe pkgs.nvd} \
+            diff /run/current-system $systemConfig
+        fi
+      '';
+    };
+  };
 }
