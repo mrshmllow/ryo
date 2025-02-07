04f3a83efc31d7dcae0119f47ba8ca500fa1f5e7
marshmallow <marshycity@gmail.com>
2025-02-07 13:11:15 +1100

    remove left over umu input

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
     nix-citizen.url = "github:LovingMelody/nix-citizen";
     nix-citizen.inputs.nix-gaming.follows = "nix-gaming";
     nix-gaming.url = "github:fufexan/nix-gaming";
-    umu.follows = "nix-gaming";
 
     wezterm.url = "github:wez/wezterm/main?dir=nix";
   };
