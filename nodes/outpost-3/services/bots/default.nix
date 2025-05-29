377185b16417ffee6dc136ff81dd25c3c4ad00e0
marshmallow <marshycity@gmail.com>
2025-05-29 11:30:41 +1000

    fix typstbot

--- nodes/outpost-3/services/bots/default.nix
+++ nodes/outpost-3/services/bots/default.nix
@@ -1,37 +1,73 @@
 {
   pkgs,
+  lib,
   inputs,
   config,
   ...
-}: let
-  dataDir = "/var/lib/typst-bot";
-in {
-  virtualisation.oci-containers.containers.actual = {
-    enable = false;
+}:
+let
+  dataDir = "/var/lib/typstbot";
 
-    image = pkgs.dockerTools.buildImage {
-      name = "hello-docker";
-      fromImage = inputs.typst-bot;
+  common = {
+    version = "locked";
+    src = inputs.typst-bot;
+    cargoHash = "sha256-zQzECKPIcUU3Pck+2OfgmZd2IWZUqQWi86wnI7lafec=";
+    nativeBuildInputs = [
+      pkgs.git
+    ];
+  };
+
+  worker = pkgs.rustPlatform.buildRustPackage (
+    finalAttrs:
+    common
+    // {
+      pname = "typst-bot-worker";
+      cargoBuildFlags = "-p worker";
+      meta.mainProgram = "worker";
+    }
+  );
+
+  package = pkgs.rustPlatform.buildRustPackage (
+    finalAttrs:
+    common
+    // {
+      pname = "typst-bot";
+      patches = [
+        ./var.patch
+      ];
+      postPatch = ''
+        substituteInPlace crates/bot/src/worker.rs \
+            --replace-fail '%TYPST_WORKER_PATH%' '${lib.getExe worker}'
+      '';
+    }
+  );
+in
+{
+  systemd.services.typstbot = {
+    script = lib.getExe package;
+    environment = {
+      DB_PATH = "${dataDir}/sqlite/db.sqlite";
+      CACHE_DIRECTORY = "${dataDir}/cache";
     };
-    volumes = [
-      "${dataDir}:/bot/sqlite"
-      "${dataDir}:/bot/cache"
-    ];
-    user = "${toString config.users.users.typstbot.uid}:${toString config.users.groups.typstbot.gid}";
-    environmentFiles = [
-      config.deployment.keys."typstbot.env".path
-    ];
+    serviceConfig = {
+      EnvironmentFile = config.deployment.keys."typstbot.env".path;
+      User = "typstbot";
+      Group = "typstbot";
+    };
+    path = [ pkgs.typst ];
   };
 
   deployment.keys."typstbot.env" = {
     keyCommand = [
       "gpg"
       "--decrypt"
-      "${../secrets/typstbot.key.gpg}"
+      "${../../../../secrets/typstbot.env.gpg}"
     ];
 
     destDir = "/etc/keys";
-    uploadAt = "pre-activation";
+    uploadAt = "post-activation";
+    user = "typstbot";
+    group = "typstbot";
   };
 
   users = {
@@ -40,12 +76,15 @@ in {
       group = "typstbot";
     };
 
-    groups.typstbot = {};
+    groups.typstbot = { };
   };
 
   systemd = {
     tmpfiles.rules = [
       "d ${dataDir} 0700 typstbot typstbot - -"
+      "d ${dataDir}/cache 0700 typstbot typstbot - -"
+      "d ${dataDir}/sqlite 0700 typstbot typstbot - -"
+      "f ${dataDir}/sqlite/db.sqlite 0700 typstbot typstbot - -"
     ];
   };
 }
