713102119a0ae3450ef6cf9903b209395b4fd42e
marshmallow <marshycity@gmail.com>
2025-07-31 17:20:47 +1000

    add qbittorrent serverConfig

--- nodes/media/services/transmission/default.nix
+++ nodes/media/services/transmission/default.nix
@@ -5,9 +5,36 @@
       enable = true;
       openFirewall = true;
       webuiPort = 8081;
-      serverConfig.Preferences.WebUI = {
-        RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
-        AlternativeUIEnabled = true;
+      serverConfig = {
+        Preferences = {
+          WebUI = {
+            RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
+            Password_PBKDF2 = "Z/bIFekWWbZbyG/sBEIPVA==:2XkBQz8C36N7nrLiX8coUJOV914lIHCLOkliN3B7/f2xbNlHS46ZW7FYQm2Yv1mygIqH5z1UE2WbBAQoRWcxmw==";
+            AlternativeUIEnabled = true;
+          };
+          # 23:00
+          Scheduler.end_time = "\0\0\0\xf\x4\xefm\x80";
+        };
+        BitTorrent.Session = {
+          AlternativeGlobalDLSpeedLimit = 1000;
+          AlternativeGlobalUPSpeedLimit = 1000;
+          UseAlternativeGlobalSpeedLimit = true;
+
+          BandwidthSchedulerEnabled = true;
+
+          AnonymousModeEnabled = true;
+
+          Preallocation = true;
+          TempPathEnabled = true;
+
+          DefaultSavePath = "/storage/data/torrents/";
+          TempPath = "/storage/data/incomplete/";
+
+          UseCategoryPathsInManualMode = true;
+
+          GlobalMaxSeedingMinutes = 8 * 1440;
+          GlobalMaxRatio = 1;
+        };
       };
     };
 
